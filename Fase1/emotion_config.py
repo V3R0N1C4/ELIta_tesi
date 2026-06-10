d0bf1949cd29a04356e5359d5becc0d44a65457a
V3R0N1C4 <veronicabosso3102@gmail.com>
2026-06-10 17:43:42 +0200

    Colori emozioni
    - ordinate le emozioni secondo la ruota di Plutchik
    - cambiati colori simili per una visualizzazione migliore

--- Fase1/emotion_config.py
+++ Fase1/emotion_config.py
@@ -1,22 +1,19 @@
 # Palette colori estesa (include le 8 base + amore e neutrale)
 EMOTION_COLORS = {
-    'gioia': '#FDD835',       # Giallo
-    'tristezza': '#1E88E5',   # Blu
-    'rabbia': '#E53935',      # Rosso
-    'paura': '#43A047',       # Verde scuro
-    'disgusto': '#8E24AA',    # Viola
-    'fiducia': '#81C784',     # Verde chiaro
-    'sorpresa': '#039BE5',    # Azzurro
-    'aspettativa': '#FB8C00', # Arancione
-    'amore': '#E91E63',       # Rosa/Magenta
-    'neutrale': '#9E9E9E'     # Grigio
+    'gioia':        '#FDD835',  # Giallo
+    'aspettativa':  '#FB8C00',  # Arancione
+    'rabbia':       '#E53935',  # Rosso
+    'disgusto':     '#8E24AA',  # Viola
+    'tristezza':    '#0000FF',  # Blu
+    'sorpresa':     '#039BE5',  # Azzurro
+    'paura':        '#228B22',  # Verde scuro
+    'fiducia':      '#81C784',  # Verde chiaro
+    'amore':        '#E91E63',  # Rosa/Magenta
+    'neutrale':     '#9E9E9E'   # Grigio
 }
 
 # Liste di definizioni colonne
-BASIC_EMOTIONS = [
-    'gioia', 'tristezza', 'rabbia', 'paura',
-    'disgusto', 'fiducia', 'sorpresa', 'aspettativa'
-]
+BASIC_EMOTIONS = [ 'gioia', 'aspettativa', 'rabbia', 'disgusto', 'tristezza', 'sorpresa', 'paura',  'fiducia']
 
 ALL_EMOTIONS = BASIC_EMOTIONS + ['amore', 'neutrale']
 
